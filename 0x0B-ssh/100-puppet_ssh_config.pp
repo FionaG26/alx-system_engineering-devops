c2ca364f84e2c973f07ac897be4506e759976be8
FionaG26 <fionamuthoni18@gmail.com>
2023-03-26 11:05:44 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,5 @@
-#This puppet manifest configures private key ~/.ssh/school
- ensure  => present,
- content => "\
   Hostname <124704-web-01>
   Username <ubuntu>
   IP adress <54.90.3.58>
   IdentityFile ~/.ssh/school
   PasswordAuthentication no
-"
-}
-
