ee5cf48d083c0397be419f8560d639d1ca42625f
FionaG26 <fionamuthoni18@gmail.com>
2023-03-26 10:07:35 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,7 @@
-#This puppet manifest configures private key ~/.ssh/school
-file { '/alx-system_engineering-devops/0x0B-ssh/100-puppet_ssh_config.pp':
-  ensure  => present,
-  mode    => '0600',
-  content => "
+
     Host 124704-web-01
       Hostname 54.90.3.58
       User ubuntu
       IdentityFile ~/.ssh/school
       PasswordAuthentication no
-  ",
-}
+  
