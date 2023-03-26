91f3e68b75dc60586e5b1d8bcd8009f90aaa0d10
FionaG26 <fionamuthoni18@gmail.com>
2023-03-26 11:42:53 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,15 @@
-  Hostname <124704-web-01>
-  Username <ubuntu>
-  IP adress <54.90.3.58>
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
+#This puppet mainfest configures using private key ~/.ssh/school
+class ssh_config {
+  file { 'alx-system_engineering-devops/0x0B-ssh/.ssh/config':
+    ensure  => file,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0644',
+    content => "# Puppet-managed file. Do not edit.\n
+Host myserver
+    HostName 54.90.3.58
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no\n",
+  }
+}
