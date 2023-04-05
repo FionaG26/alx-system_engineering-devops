b6634771758487aaabbf0b8351b5db864e254568
FionaG26 <fionamuthoni18@gmail.com>
2023-04-04 23:34:18 -0700

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,7 @@
 class ssh_config {
   file { 'alx-system_engineering-devops/0x0B-ssh/.ssh/config':
     ensure  => file,
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0644',
     content => "# Puppet-managed file. Do not edit.\n
-Host myserver
     HostName 54.90.3.58
     User ubuntu
     IdentityFile ~/.ssh/school
