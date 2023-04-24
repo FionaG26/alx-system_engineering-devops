d36c6bbbcea7bc5b31b78860fba6d6a981e1205f
FionaG26 <fionamuthoni18@gmail.com>
2023-04-24 17:39:23 +0000

    tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""
-Script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress and exports
-the data to a CSV file.
-"""
+
+"""Import libraries"""
+
 import csv
 import requests
-from sys import argv
+import sys
 
+
+"""Import libraries"""
 
 if __name__ == "__main__":
-    user_id = argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todo_url = ("https://jsonplaceholder.typicode.com/todos?"
-                "userId={}".format(user_id))
+    user_id = int(sys.argv[1])
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    user_response = requests.get(user_url).json()
-    todo_response = requests.get(todo_url).json()
+    file_content = []
 
-    total_tasks = len(todo_response)
-    completed_tasks = sum(task.get("completed") for task in todo_response)
-    user_name = user_response.get("username")
+    todo_data = requests.get(todos_url).json()
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_name, completed_tasks, total_tasks))
+    employee_name = requests.get(users_url).json()["username"]
 
-    filename = "{}.csv".format(user_id)
-    with open(filename, mode='w') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME",
-                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todo_response:
-            writer.writerow([user_id, user_name,
-                            task.get("completed"), task.get("title")])
+    for todo in todo_data:
+        if user_id == todo["userId"]:
+            file_content.append(
+                [str(user_id), employee_name, todo["completed"],
+                 todo["title"]])
+
+    print(file_content)
+    file_name = "{}.csv".format(user_id)
+    with open(file_name, 'w', newline='') as csv_file:
+        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for row in file_content:
+            for item in row:
+                str(item)
+            write.writerow(row)
