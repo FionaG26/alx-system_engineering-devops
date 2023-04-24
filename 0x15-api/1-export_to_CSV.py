4babf150fa66e0128e1e804a5d8ed95efd957fbd
FionaG26 <fionamuthoni18@gmail.com>
2023-04-24 15:16:29 +0000

    tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress and export
@@ -15,22 +16,27 @@ if __name__ == "__main__":
     employee_id = argv[1]
 
     # Get the employee's name
-    user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                                 .format(employee_id))
+    user_response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
     # Get the employee's tasks
-    tasks_response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                                  .format(employee_id))
+    tasks_response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(employee_id))
     tasks_data = tasks_response.json()
 
     # Export data to CSV file
     with open("{}.csv".format(employee_id), mode="w", newline="") as csv_file:
-        writer = csv.writer(csv_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(
+            csv_file,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
 
         for task in tasks_data:
             task_status = "True" if task.get("completed") else "False"
             task_title = task.get("title")
-            writer.writerow([employee_id, employee_name, task_status, task_title])
-
+            writer.writerow([employee_id, employee_name,
+                            task_status, task_title])
