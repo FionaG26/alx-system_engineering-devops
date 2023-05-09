f436e7cc256d62a94dc6b5f41062d9f7bb877862
FionaG26 <fionamuthoni18@gmail.com>
2023-05-09 12:41:14 +0000

    first hot 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
 """
-1-top_ten
+Queries the Reddit API and prints the titles of the first 10 hot posts listed
+for a given subreddit.
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to search.
-
-    Returns:
-        None.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
-        data = response.json().get("data")
-        if data:
-            children = data.get("children")
-            for child in children:
-                print(child.get("data").get("title"))
-        else:
-            print("No data found.")
-    elif response.status_code == 302:
-        print("None")
+        posts = response.json().get('data').get('children')
+        for post in posts:
+            print(post.get('data').get('title'))
     else:
-        print("Error: {}".format(response.status_code))
-
-
-if __name__ == "__main__":
-    subreddit = input("Enter the name of the subreddit to search: ")
-    top_ten(subreddit)
+        print(None)
