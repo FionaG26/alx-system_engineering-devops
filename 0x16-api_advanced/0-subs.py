40242da3d7ab710e46f2f528355ed981078db31c
FionaG26 <fionamuthoni18@gmail.com>
2023-05-09 11:22:02 +0000

    subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,16 +7,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for the given subreddit.
-    If an invalid subreddit is given, returns 0.
-    """
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """returns number of total subscribers"""
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        return data.get('subscribers')
+    if response.status_code != 200:
+        return (0)
+    response = response.json()
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
+
     else:
-        return 0
+        return (0)
