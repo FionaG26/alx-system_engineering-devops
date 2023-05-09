8c095af37b320d5f578a1d03c2c7777661ecff08
FionaG26 <fionamuthoni18@gmail.com>
2023-05-09 13:26:46 +0000

    reddit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,61 +1,35 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and counts the occurrences of
-given keywords in the titles of the hot posts of a given subreddit.
+Recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords
 """
-
 import requests
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-def count_words(subreddit, word_list, after=None, word_count={}):
-    """Counts the occurrences of given keywords in the titles of the hot posts of
-    a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list of str): The list of keywords to count.
-        after (str): The Reddit post ID to start the search from.
-        word_count (dict): The dictionary to store the count of each keyword.
-
-    Returns:
-        None.
-    """
-
-    if not word_list:
-        # No more keywords to count, print the result
-        for word, count in sorted(word_count.items(),
-                                   key=lambda x: (-x[1], x[0])):
-            print('{}: {}'.format(word, count))
-        return
-
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
+def count_words(subreddit, word_list, after=None, count_dict=None):
+    if count_dict is None:
+        count_dict = {}
+    if after is None:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    else:
+        url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        # Invalid subreddit or other error
         return
-
     data = response.json()
-    posts = data.get('data', {}).get('children', [])
-
-    for post in posts:
-        title = post.get('data', {}).get('title', '').lower()
-
-        for word in word_list[:]:
-            if (' ' + word + ' ') in (' ' + title + ' '):
-                # Found a keyword in the title, increment the count
-                word_list.remove(word)
-                word_count[word] = word_count.get(word, 0) + 1
-
-    if not posts:
-        # No more posts to search, print the result
-        for word, count in sorted(word_count.items(),
-                                   key=lambda x: (-x[1], x[0])):
-            print('{}: {}'.format(word, count))
-        return
-
-    # Recursively search the remaining posts
-    count_words(subreddit, word_list, posts[-1]['data']['name'], word_count)
+    articles = data['data']['children']
+    for article in articles:
+        title = article['data']['title'].lower()
+        for word in word_list:
+            if word.lower() in title and not any(ch.isalpha() for ch in title[title.index(word.lower())-1: title.index(word.lower())]) and not any(ch.isalpha() for ch in title[title.index(word.lower())+len(word): title.index(word.lower())+len(word)+1]):
+                if word.lower() in count_dict:
+                    count_dict[word.lower()] += 1
+                else:
+                    count_dict[word.lower()] = 1
+    after = data['data']['after']
+    if after is not None:
+        count_words(subreddit, word_list, after, count_dict)
+    else:
+        sorted_count = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_count:
+            print("{}: {}".format(word, count))
