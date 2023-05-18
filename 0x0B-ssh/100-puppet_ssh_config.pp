f73bf82884284c38aed84f1c8679831c928e42eb
FionaG26 <fionamuthoni18@gmail.com>
2023-05-18 09:37:07 -0700

    keys

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #This puppet mainfest configures using private key ~/.ssh/school
-class ssh_config {
-  file { 'alx-system_engineering-devops/0x0B-ssh/.ssh/config':
-    ensure  => file,
-    content => "# Puppet-managed file. Do not edit.\n
-    HostName 54.90.3.58
-    User ubuntu
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no\n",
-  }
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
