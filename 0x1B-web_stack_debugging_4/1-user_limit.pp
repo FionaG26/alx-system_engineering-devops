ecf3400fd69627fcf40e4871097a6f625f521517
FionaG26 <fionamuthoni18@gmail.com>
2023-05-20 17:13:17 +0000

    1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -13,4 +13,3 @@ exec { 'change-os-configuration-for-holberton-user':
   command => '/bin/sh -c "echo \"holberton soft nofile 4096\" >> /etc/security/limits.conf && echo \"holberton hard nofile 4096\" >> /etc/security/limits.conf"',
   unless  => '/bin/grep -q "holberton soft nofile 4096" /etc/security/limits.conf && /bin/grep -q "holberton hard nofile 4096" /etc/security/limits.conf',
 }
-
